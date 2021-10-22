e31cf96d30b40b973e92c3ea9966c41035e367a0
Faqih Muntashir <itsfaqih@gmail.com>
2021-10-22 23:23:05 +0700

    refactor: change how scroll navigation works

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,7 +6,15 @@ const navbar = document.getElementById('navbar');
 const topPadding = 16 * 1.75;
 
 function addNavbarBackgroundOnScroll() {
-  const classes = ['fixed', 'bg-white', 'bg-opacity-80', 'backdrop-filter', 'backdrop-blur-xl', 'border-b', 'border-gray-300'];
+  const classes = [
+    'fixed',
+    'bg-white',
+    'bg-opacity-80',
+    'backdrop-filter',
+    'backdrop-blur-xl',
+    'border-b',
+    'border-gray-300',
+  ];
   if (document.body.scrollTop > topPadding || document.documentElement.scrollTop > topPadding) {
     navbar.classList.add(...classes);
   } else {
@@ -30,11 +38,11 @@ function toggleMobileMenu(open) {
   if (open) {
     html.classList.add('overflow-y-hidden');
     body.classList.add('overflow-y-hidden');
-    mobileMenu.classList.remove('translate-x-full')
+    mobileMenu.classList.remove('translate-x-full');
   } else {
     html.classList.remove('overflow-y-hidden');
     body.classList.remove('overflow-y-hidden');
-    mobileMenu.classList.add('translate-x-full')
+    mobileMenu.classList.add('translate-x-full');
   }
 }
 
@@ -45,11 +53,11 @@ window.onresize = function () {
   }
 };
 
-document.querySelectorAll('[href^="#"]').forEach(link => {
-  link.onclick = function (event) {
+document.addEventListener('click', event => {
+  if (event.target.href && event.target.href.includes('#') || event.target.dataset.target) {
     event.preventDefault();
     const tagName = event.target.tagName;
-    const href = tagName === 'A' ? event.target.href : event.target.parentElement.href;
+    const href = tagName === 'A' ? event.target.href : event.target.dataset.target;
     const targetId = href.slice(href.indexOf('#') + 1);
     const offsetTop = document.getElementById(targetId).offsetTop;
     let target = offsetTop;
@@ -67,5 +75,5 @@ document.querySelectorAll('[href^="#"]').forEach(link => {
     if (isMobileMenu) {
       toggleMobileMenu(false);
     }
-  };
+  }
 });
