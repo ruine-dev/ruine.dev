447255ecb0349e6ca4fe889a8828226fbc98bd49
Faqih Muntashir <itsfaqih@gmail.com>
2021-10-28 08:44:00 +0700

    feat: change font to Manrope

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Work Sans', ...defaultTheme.fontFamily.sans],
+        sans: ['Manrope', ...defaultTheme.fontFamily.sans],
       },
       zIndex: {
         '-10': '-10',
